e4ada486e9bdbcb91d900d09537738a3d095db14
alliestehney <alliestehney1@gmail.com>
2017-10-20 15:51:19 -0400

    Added more functions and fixed the lives feature for Grant

--- game.js
+++ game.js
@@ -1,43 +1,52 @@
-var play = prompt("Would you like to play a game with two characters? (yes or no)");
+function startGame() {
+	var play = prompt("Would you like to play a game with two characters? (yes or no)");
 
-if (play === "yes") {
-	var name = prompt("Please enter your name.");
-	console.log(name);
+	if (play === "yes") {
+		var name = prompt("Please enter your name.");
+		console.log(name);
+		startCombat();
+	}
 }
-
-var usrpoints = 40;
-var grantpoints = 10;
-// var random = (Math.floor(Math.random() *2) + 1);
-var grantWins = 0;
-var usrWins = 0;
-var grantLosses = 0;
+startGame();
 
 function getDamage() {
-	return Math.floor(Math.random() *2) + 1;
+	return Math.floor(Math.random() *5) + 1;
 }
 
-while (usrpoints !== 0 && grantpoints !== 0 ) {
-	usrpoints -= getDamage();
-	grantpoints -= getDamage();
-	console.log(name + " has " + usrpoints + " health points left.");
-	console.log("Grant Almighty has " + grantpoints + " health points left.");
-	if (grantpoints === 0) {
-		grantLosses ++;
+function startCombat() {
+
+	var usrpoints = 40;
+	var grantpoints = 10;
+	var grantWins = 0;
+	var usrWins = 0;
+	var grantLosses = 0;
+
+	while (usrpoints > 0 && grantpoints > 0 ) {
+		usrpoints -= getDamage();
+		grantpoints -= getDamage();
+		console.log(name + " has " + usrpoints + " health points left.");
+		console.log("Grant Almighty has " + grantpoints + " health points left.");
+		if (grantpoints <= 0) {
+			grantLosses ++;
+		}
+		if (0 >= grantpoints && grantLosses < 3) {
+			grantpoints = 10;
+			console.log("WORKED!!!!")
+		}
 	}
-	if (grantpoints === 0 && grantLosses < 3) {
-		grantpoints = 10;
-		// console.log("WORKED!!!!")
+
+	if (0 <= usrpoints) {
+		grantWins ++;
+		console.log(name + " has lost the game.")
+	} else {
+		usrWins ++;
+		console.log("Grant Almighty has lost the game.")
 	}
-}
 
-if (urspoints = 0) {
-	grantWins ++;
-	console.log(name + " has lost the game.")
-} else {
-	usrWins ++;
-	console.log("Grant Almighty has lost the game.")
+
+	console.log("TOTAL WINS FOR GRANT: " + grantWins);
+	console.log("TOTAL WINS FOR " + name +": " + usrWins);
 }
 
 
-console.log("TOTAL WINS FOR GRANT: " + grantWins);
-console.log("TOTAL WINS FOR " + name +": " + usrWins);
+
